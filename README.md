2df3dc8276ede77e5fd732324556f5f00d66c06a
Inky1003 <inky1003@gmail.com>
2020-04-08 22:27:48 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# SimpleTextProgressBar
-A simple text progress bar
+# SimpleTextProgressBar - A simple progress bar for simple projects
+This Text Progress Bar is made for projects that don't need great, big, confusing, etc. progress bars. It's code is only 3 KB, and It provides any progress bar relative size, with any relative value, with any prefix before the Progress Bar.
+
+## How to use?
+Just
+
+> simpletextprogressbar.set_progress_bar()
+
+This function sends a unfinished line to the console.
+
+> simpletextprogressbar.change_position(position, size, optional_prefix)
+
+This function sets a new progress bar in the relative position from the relative size, and also prints a text before the progress bar, If specified.
+
+
+Enjoy!
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpletextprogressbar", # Replace with your own username
+    version="1.1.0",
+    author="Inky",
+    author_email="inky1003@gmail.com",
+    description="A simple progress bar for simple projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3.0-only",
+        "Operating System :: OS Independent",
+    ],
+    keywords='progress bar command line console text window simple lightweight',
+    python_requires='>=3.5',
+)
